f98e356be79f66c28767a4df1fb69fa45da8482c
Rishav Ghosh <rishavarighs@gmail.com>
2026-04-26 22:55:12 +0530

    Static image rendering

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static('Images'));
+
 app.use(cors());
+const path = require('path');
+app.use('/images', express.static(path.join(__dirname, 'Images')));
 // initialiseDatabase(); //commented out required only for local use connection
 const PORT = process.env.PORT || 5000;
 
